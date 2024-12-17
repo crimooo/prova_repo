dd1653041e869d91734e0ef488c38f8adc76f6c5
Mora-Cristian <mora.cristian.08@itisdalmine.edu.it>
2024-12-17 10:01:17 +0100

    implementazione menu'

--- menu.py
+++ menu.py
@@ -1,1 +1,17 @@
-print(Hello)
+import calcolatrice as clc
+
+while True:
+    print()
+    print("Benvenuti nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1) sottrazione \n2) addizione \n 0) exit \n"))
+    if scelta == 0:
+        break
+    elif scelta == 1:
+        clc.sottrazione
+        
+    elif scelta == 2:
+        clc.somma
+
+    else:
+        print("valore non valido")
