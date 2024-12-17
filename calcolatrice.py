90b88fd76a051dc9c1301fe188dfe714e67799f8
Mora-Cristian <mora.cristian.08@itisdalmine.edu.it>
2024-12-17 09:41:04 +0100

    calcolatrice sistemata

--- calcolatrice.py
+++ calcolatrice.py
@@ -3,7 +3,9 @@ while scelta != 0:
     print("Benvenuti nella nostra calcolatrice")
     print("Inserisci l'operazione che vuoi effettuare")
     scelta = int(input("1) sottrazione \n2) addizione \n 0) exit \n"))
-    if scelta == 1:
+    if scelta == 0:
+        break
+    elif scelta == 1:
         n1 = int(input("inserisci primo numero: "))
         n2 = int(input("inserisci secondo numero: "))
         print(f"{n1} - {n2} = {n1-n2}")
