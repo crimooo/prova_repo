d45a02d1c48747dc3b0bf524114a5eefc0e348b4
crimooo <moracrist2008@gmail.com>
2024-12-17 09:58:13 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,15 @@
-print(Hello)
+
+while True:
+    print()
+    print("Benvenuti nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1) sottrazione \n2) addizione \n 0) exit \n"))
+    if scelta == 0:
+        break
+    elif scelta == 1:
+        sottrazione()
+    elif scelta == 2:
+        somma()
+
+    else:
+        print("valore non valido")
