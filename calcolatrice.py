7961522f22b8df6808bf0644648ee8d883e20a83
Mora-Cristian <mora.cristian.08@itisdalmine.edu.it>
2024-12-17 09:47:37 +0100

    implementata la sottrazione

--- calcolatrice.py
+++ calcolatrice.py
@@ -3,17 +3,21 @@ def somma():
     n2 = int(input("inserisci secondo numero: "))
     print(f"{n1} + {n2} = {n1+n2}")
 
+def sottrazione():
+    n1 = int(input("inserisci primo numero: "))
+    n2 = int(input("inserisci secondo numero: "))
+    print(f"{n1} - {n2} = {n1-n2}")
+
+
 while True:
+    print()
     print("Benvenuti nella nostra calcolatrice")
     print("Inserisci l'operazione che vuoi effettuare")
     scelta = int(input("1) sottrazione \n2) addizione \n 0) exit \n"))
     if scelta == 0:
         break
     elif scelta == 1:
-        n1 = int(input("inserisci primo numero: "))
-        n2 = int(input("inserisci secondo numero: "))
-        print(f"{n1} - {n2} = {n1-n2}")
-
+        sottrazione()
     elif scelta == 2:
         somma()
 
