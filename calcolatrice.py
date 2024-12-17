fe61446a577c7685b7ae9c9a8c71b3a26dfc92d5
Mora-Cristian <mora.cristian.08@itisdalmine.edu.it>
2024-12-17 09:38:56 +0100

    implementazione calcolatrice

---
+++ calcolatrice.py
@@ -0,0 +1,17 @@
+scelta = -1
+while scelta != 0:
+    print("Benvenuti nella nostra calcolatrice")
+    print("Inserisci l'operazione che vuoi effettuare")
+    scelta = int(input("1) sottrazione \n2) addizione \n 0) exit \n"))
+    if scelta == 1:
+        n1 = int(input("inserisci primo numero: "))
+        n2 = int(input("inserisci secondo numero: "))
+        print(f"{n1} - {n2} = {n1-n2}")
+
+    elif scelta == 2:
+        n1 = int(input("inserisci primo numero: "))
+        n2 = int(input("inserisci secondo numero: "))
+        print(f"{n1} + {n2} = {n1+n2}")
+
+    else:
+        print("valore non valido")
