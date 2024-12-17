6ee581505796773bb3d8e37c969a16acb587cfab
Mora-Cristian <mora.cristian.08@itisdalmine.edu.it>
2024-12-17 09:45:20 +0100

    calcolatrice finale

--- calcolatrice.py
+++ calcolatrice.py
@@ -1,7 +1,7 @@
 def somma():
     n1 = int(input("inserisci primo numero: "))
-        n2 = int(input("inserisci secondo numero: "))
-        print(f"{n1} + {n2} = {n1+n2}")
+    n2 = int(input("inserisci secondo numero: "))
+    print(f"{n1} + {n2} = {n1+n2}")
 
 while True:
     print("Benvenuti nella nostra calcolatrice")
